b92d125226596f3dbbf977b6876ed2a4cd1e698f
huseyyinsahin <huseysahin19@outlook.com>
2025-02-17 21:45:41 +0300

    index.js

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ require("express-async-errors");
 
 /* ------------------------------------------------------- */
 
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
+
+/* ------------------------------------------------------- */
+
+app.use(express.json());
+
+app.use(require("./src/middlewares/logger"));
+
+app.use(require("./src/middlewares/authentication"));
+
+app.use(require("./src/middlewares/queryHandler"));
 
 /* ------------------------------------------------------- */
 
