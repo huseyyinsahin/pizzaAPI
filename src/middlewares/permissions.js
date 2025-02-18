3e96d8d3a5bdaa785f6e9d12684f01895282384d
huseyyinsahin <huseysahin19@outlook.com>
2025-02-18 19:19:04 +0300

    permission

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -9,4 +9,12 @@ module.exports = {
       throw new Error("NoPermission: You must login.");
     }
   },
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You are not admin");
+    }
+  },
 };
